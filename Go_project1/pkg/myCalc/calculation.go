a0b957068ee1c27ac3dc54b94f37f2ce419d3ba9
artmtsh <58192947+artmtsh@users.noreply.github.com>
2023-10-24 21:49:44 +0300

    Changes

--- Go_project1/pkg/myCalc/calculation.go
+++ Go_project1/pkg/myCalc/calculation.go
@@ -9,15 +9,9 @@ func Calculation(num1 float32, operation string, num2 float32) float32 {
 	case "-":
 		return num1 - num2
 	case "*":
-		fmt.Println(num1 * num2)
 		return num1 * num2
 	case "/":
-		if num2 == 0 {
-			fmt.Println("Деление на ноль недопустимо.")
-			return 0.0
-		} else {
-			return num1 / num2
-		}
+		return num1 / num2
 	default:
 		fmt.Println("Некорректная операция. Пожалуйста, используйте символы +, -, * или /.")
 	}
