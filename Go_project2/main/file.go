0adb32df0d4f2567002bbebde106baf61581eb95
artmtsh <58192947+artmtsh@users.noreply.github.com>
2023-10-26 18:13:44 +0300

    Update findKthLargest

--- Go_project2/main/file.go
+++ Go_project2/main/file.go
@@ -4,16 +4,16 @@ func findKthLargest(nums []int, k int) int {
 	if k < 1 || k > len(nums) {
 		return -1
 	}
-	for i := 1; i < k; i++ {
-		maxVal := nums[i]
+
+	for i := 0; i < k; i++ {
 		maxIdx := i
 		for j := i + 1; j < len(nums); j++ {
-			if nums[j] > maxVal {
-				maxVal = nums[j]
+			if nums[j] > nums[maxIdx] {
 				maxIdx = j
 			}
 		}
 		nums[i], nums[maxIdx] = nums[maxIdx], nums[i]
 	}
+
 	return nums[k-1]
 }
