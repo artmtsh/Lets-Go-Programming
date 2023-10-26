2a3a8b7bb4bf0b6d888dfa8807adc02c998f10c2
artmtsh <58192947+artmtsh@users.noreply.github.com>
2023-10-26 16:09:21 +0300

    Rewrite without sort

--- Go_project2/main/file.go
+++ Go_project2/main/file.go
@@ -1,11 +1,17 @@
 package main
 
-import (
-	"sort"
-)
-
 func findKthLargest(nums []int, k int) int {
-	sort.Ints(nums)
-	return nums[len(nums)-k]
-	//add
+	if k < 0 || k > len(nums) {
+		return -1
+	}
+	maxVals := make([]int, k)
+	for _, v := range nums {
+		if v >= maxVals[k-1] {
+			for j := 0; j < k-1; j++ {
+				maxVals[j] = maxVals[j+1]
+			}
+			maxVals[k-1] = v
+		}
+	}
+	return maxVals[0]
 }
