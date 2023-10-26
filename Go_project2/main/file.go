1e876442b3e03ee605b3319e973f60e3ab4c64cc
artmtsh <58192947+artmtsh@users.noreply.github.com>
2023-10-26 13:49:33 +0300

    Delete reverse from func

--- Go_project2/main/file.go
+++ Go_project2/main/file.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"sort"
 )
 
@@ -9,9 +8,4 @@ func findKthLargest(nums []int, k int) int {
 	sort.Ints(nums)
 	return nums[len(nums)-k]
 	//add
-}
-
-func main() {
-	nums := []int{3, 2, 3, 1, 2, 4, 5, 5, 6}
-	fmt.Println(findKthLargest(nums, 4))
 }
