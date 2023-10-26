5851c0a0ba23636fa9846e1d84b9f075a2a51190
artmtsh <58192947+artmtsh@users.noreply.github.com>
2023-10-26 13:17:17 +0300

    Delete unique

--- Go_project2/main/file.go
+++ Go_project2/main/file.go
@@ -5,27 +5,10 @@ import (
 )
 
 func findKthLargest(nums []int, k int) int {
-	var uniqueSlice []int
-	if k > len(nums) {
-		return -1000000
+	sort.Ints(nums)
+	for i, j := 0, len(nums)-1; i < j; i, j = i+1, j-1 {
+		nums[i], nums[j] = nums[j], nums[i]
 	}
-	for _, v := range nums {
-		found := false
-		for _, u := range uniqueSlice {
-			if u == v {
-				found = true
-				break
-			}
-		}
-		if !found {
-			uniqueSlice = append(uniqueSlice, v)
-		}
-	}
-	sort.Ints(uniqueSlice)
-	for i, j := 0, len(uniqueSlice)-1; i < j; i, j = i+1, j-1 {
-		uniqueSlice[i], uniqueSlice[j] = uniqueSlice[j], uniqueSlice[i]
-	}
-
-	return uniqueSlice[k-1]
+	return nums[k-1]
 	//add
 }
