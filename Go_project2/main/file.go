6d52b3272dcf18e55cbd83c86125c840ec754ca9
artmtsh <58192947+artmtsh@users.noreply.github.com>
2023-10-27 14:56:44 +0300

    Update findKthLargest with partition

--- Go_project2/main/file.go
+++ Go_project2/main/file.go
@@ -1,28 +1,33 @@
 package main
 
-import "math"
-
 func findKthLargest(nums []int, k int) int {
 	if k < 1 || k > len(nums) {
 		return -1
 	}
-	kthLargest := math.MinInt
-	for k > 0 {
-		currentMax := math.MinInt
-		maxIndex := -1
-		for i, num := range nums {
-			if num > currentMax {
-				currentMax = num
-				maxIndex = i
-			}
-		}
-		if maxIndex >= 0 {
-			nums = append(nums[:maxIndex], nums[maxIndex+1:]...)
-			kthLargest = currentMax
-			k--
+	k--
+	for {
+		pivotIndex := partition(nums)
+		if k == pivotIndex {
+			return nums[k]
+		} else if k < pivotIndex {
+			nums = nums[:pivotIndex]
 		} else {
-			break
+			nums = nums[pivotIndex+1:]
+			k -= (pivotIndex + 1)
 		}
 	}
-	return kthLargest
+}
+
+func partition(nums []int) int {
+	pivotIndex := len(nums) - 1
+	pivot := nums[pivotIndex]
+	left := 0
+	for i := 0; i < pivotIndex; i++ {
+		if nums[i] > pivot {
+			nums[i], nums[left] = nums[left], nums[i]
+			left++
+		}
+	}
+	nums[left], nums[pivotIndex] = nums[pivotIndex], nums[left]
+	return left
 }
